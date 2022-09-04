a5402e23d5cb4b64994ad41141b2f25495914911
Sri Charan <charankonduri97@gmail.com>
2022-09-04 20:47:28 +0530

    Refactor: modify the ViewModelKey implementation back to kotlin from java

--- app/src/main/java/com/apptimistiq/android/fitstreak/di/ViewModelFactory.kt
+++ app/src/main/java/com/apptimistiq/android/fitstreak/di/ViewModelFactory.kt
@@ -3,9 +3,11 @@ package com.apptimistiq.android.fitstreak.di
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import dagger.Binds
+import dagger.MapKey
 import dagger.Module
 import javax.inject.Inject
 import javax.inject.Provider
+import kotlin.reflect.KClass
 
 /**
  * ViewModelFactory which uses Dagger to create the instances.
@@ -51,11 +53,9 @@ abstract class ViewModelBuilderModule {
 }
 
 
-/*
 @Target(AnnotationTarget.FUNCTION, AnnotationTarget.PROPERTY_GETTER, AnnotationTarget.PROPERTY_SETTER)
 @Retention(AnnotationRetention.RUNTIME)
 @MapKey
 annotation class ViewModelKey(val value : KClass<out ViewModel>)
-*/
 
 
